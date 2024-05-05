73d0abd5ff9f309fc2ea00cd51933b57b7a701bc
zawadinjagi <zawadinjagi619@gmail.com>
2024-05-06 00:12:24 +0300

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import About from "../components/About"; 
+import Home from "../components/Home"; 
+import NavBar from "../components/NavBar"; 
 function App() {
   return (
     <div>
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Liza";
+export const city = "New York";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+
 
 // export all three variables as named exports
